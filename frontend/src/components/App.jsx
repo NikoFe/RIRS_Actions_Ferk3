d58c100d6ce09a6b61f590011ad11f298bfec3e2
Niko <niko.ferk@stdudent.um.si>
2025-01-28 21:18:31 +0100

    frontend1

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -90,6 +90,15 @@ const App = () => {
 
   return (
     <div>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+
       {user ? (
         <>
           <h1>Welcome, {user.username}</h1>
